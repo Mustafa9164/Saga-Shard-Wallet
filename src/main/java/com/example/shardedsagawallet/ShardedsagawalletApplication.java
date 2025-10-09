4dda0c9955e61337013d5bf8c8b1b322feb48da8
Md Mustafa <104555143+Mustafa9164@users.noreply.github.com>
2025-10-09 08:50:34 +0530

    Update ShardedsagawalletApplication.java

--- src/main/java/com/example/shardedsagawallet/ShardedsagawalletApplication.java
+++ src/main/java/com/example/shardedsagawallet/ShardedsagawalletApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShardedsagawalletApplication {
 
 	public static void main(String[] args) {
+		System.out.println("start");
 		SpringApplication.run(ShardedsagawalletApplication.class, args);
+		System.out.println("end");
 	}
 
 }
