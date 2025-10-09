7f3b58801cbf272e14e8f61cf82ea8a1e049e4a8
Mustafa9164 <mohdmustafa9164@gmail.com>
2025-10-09 08:55:16 +0530

    added x var

--- src/main/java/com/example/shardedsagawallet/ShardedsagawalletApplication.java
+++ src/main/java/com/example/shardedsagawallet/ShardedsagawalletApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShardedsagawalletApplication {
 
 	public static void main(String[] args) {
+		int x=10;
 		SpringApplication.run(ShardedsagawalletApplication.class, args);
 	}
 
